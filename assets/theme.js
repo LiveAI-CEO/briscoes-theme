13ac29fa783c10f9397a3bc1ecf8a2c1b2f56745
LiveAI-CEO <liveaimkt@gmail.com>
2026-07-03 13:22:13 -0400

    Add safe theme JavaScript placeholder

---
+++ assets/theme.js
@@ -0,0 +1,10 @@
+/*
+  Briscoe's Kitchen & Spice theme JavaScript placeholder.
+  The layout loads this file so future enhancements can be added without creating a missing asset request.
+*/
+
+(function () {
+  'use strict';
+
+  document.documentElement.classList.add('bk-js-ready');
+})();
